00fd0a5e784a00595a29eb8baa18ebe6d4828a08
varshv596-del <varshv596@gmail.com>
2026-07-27 15:01:10 +0530

    Add Number Guessing Game project

--- dice-roller/main.py
+++ dice-roller/main.py
@@ -1,15 +1,1 @@
-import random
 
-print("🎲 Welcome to Dice Roller!")
-
-while True:
-    input("\nPress Enter to roll the dice...")
-
-    dice = random.randint(1, 6)
-    print(f"You rolled: {dice}")
-
-    choice = input("Roll again? (y/n): ").lower()
-
-    if choice != "y":
-        print("\n👋 Thanks for playing!")
-        break
